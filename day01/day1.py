5f00fe0d38ee6b2a8d3134ee3366eeb6b84c6441
AdamZed <adamzanon@hotmail.com>
2022-12-03 17:20:13 -0500

    day 1

---
+++ day01/day1.py
@@ -0,0 +1,26 @@
+class Solution:
+
+    def __init__(self, fname):
+        self.data = self.parse_file(fname)
+        
+        print(f'Part 1: {self.part1()}')
+        print(f'Part 2: {self.part2()}')
+
+    def parse_file(self, fname):
+        with open(fname) as f:
+            return [[int(cal) for cal in elf.split('\n')] for elf in f.read().strip().split('\n\n')]
+            
+    def part1(self):
+        return max(sum(elf) for elf in self.data)
+
+    def part2(self):
+        import heapq
+        cals = [sum(elf) for elf in self.data]
+        heapq.heapify(cals)
+        return sum(heapq.nlargest(3, cals))
+
+
+if __name__ == "__main__":
+    import sys
+    filename = 'input.txt' if len(sys.argv) <= 1 else sys.argv[1] 
+    Solution(filename)
